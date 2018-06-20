49f3afad76fd5cd18c3a26f905a3644ab7b30e2a
Bill <office@ourweb.net>
2018-06-20 11:17:17 -0400

    zip code update

---
+++ WebAPI1/Controllers/ZipRadiusController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPI1.Models;
+
+namespace WebAPI1.Controllers
+{
+    public class ZipRadiusController : ApiController
+    {
+        private Model1 db = new Model1();
+        private const string ZIPAPIKEY = "haHmbNrE9Gg3G8EqqrqTvXSsXp7BGEn4u84Bz4MLqA7VqlVPBLjij0ezIVm5f9Wf";
+        private const string ZIPURL = "https://www.zipcodeapi.com/rest/" + ZIPAPIKEY + "/radius.json/";
+    }
+}
